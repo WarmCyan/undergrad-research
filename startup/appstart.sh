ec0806cbd9dfde446abb94a97fbaa636e445eeb9
Nathan Martindale <nmblenderdude0@gmail.com>
2018-02-08 22:30:04 -0600

    Added missing ampersand in app startup script

--- startup/appstart.sh
+++ startup/appstart.sh
@@ -10,4 +10,4 @@ chromium https://github.com/WildfireXIII/undergrad-research &>/dev/null &
 chromium https://github.com/openai/gym &>/dev/null &
 chromium https://gym.openai.com/docs/ &>/dev/null &
 
-chromium https://www.tensorflow.org/api_docs/python/ 
+chromium https://www.tensorflow.org/api_docs/python/ &
