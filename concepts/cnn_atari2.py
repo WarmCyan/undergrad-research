77c45d1dc84e7f5d321b5284866e6cdf6d6e892c
Nathan Martindale <nmblenderdude0@gmail.com>
2018-02-13 21:11:09 -0600

    Progress on dqn

--- concepts/cnn_atari2.py
+++ concepts/cnn_atari2.py
@@ -4,6 +4,10 @@ import numpy as np
 #from scipy.misc import imresize
 from skimage.transform import resize
 
+
+# https://github.com/aymericdamien/TensorFlow-Examples/blob/master/examples/3_NeuralNetworks/convolutional_network.py 
+
+
 # create an environment
 
 class Environment:
@@ -12,11 +16,21 @@ class Environment:
         #state_0 = env.reset()
 
     def preprocessFrame(self, frame):
-
         greyFrame = np.dot(frame[...,:3], [.3, .6, .1]) # convert to grey scale? TODO: don't think that's actually what it does?
-        
-        return resize(greyFrame, (64,64))
+        return resize(greyFrame, (84,84))
         
 
 class Agent:
     def __init__():
+        self.sess = tf.Session()
+
+
+        self.input = tf.placeholder(tf.float32, shape=(84,84,4)) # TODO: pretty sure that shape isn't right
+
+        # 32 filters
+        # TODO: kernel size (specified 8x8) is 8?
+        # stride of 4
+        self.conv1 = tf.layers.conv2d(self.input, 32, 8, 4, activation=tf.nn.relu)
+        
+        pass
+
