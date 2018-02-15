4c670d8751fd8ffbb4983b65b5822f90c5a97320
Nathan Martindale <nmblenderdude0@gmail.com>
2018-02-15 11:20:08 -0600

    Started adding epsilon to agent

--- concepts/cnn_atari2.py
+++ concepts/cnn_atari2.py
@@ -5,7 +5,8 @@ import numpy as np
 from skimage.transform import resize
 from skimage.color import rgb2grey
 from skimage.io import imsave
-from skimage.util import crop
+
+import random
 
 
 # https://github.com/aymericdamien/TensorFlow-Examples/blob/master/examples/3_NeuralNetworks/convolutional_network.py 
@@ -25,32 +26,37 @@ class Environment:
         #state_0 = env.reset()
         print("Environment initialized")
 
+    def getInitialState(self):
+        return self.preprocessFrame(self.env.reset())
+
     def preprocessFrame(self, frame):
         frame = resize(frame, (110,84))
         frame = frame[18:102,0:84]
-        return rgb2grey(frame)
+        frame = rgb2grey(frame)
+        frame = np.reshape(frame, (1,84,84,1))
+        return frame
 
     def act(self, action):
         observation, reward, done, info = self.env.step(action)
         if done: print("DONE")
 
-        return observation, reward, done
+        observationFrame = self.preprocessFrame(observation)
+        return observationFrame, reward, done
+
 
 class Agent:
     def __init__(self):
         self.sess = tf.Session()
+        self.epsilon = 1.0
         print("agent initialized")
 
 
     def buildGraph(self):
-        #self.input = tf.placeholder(tf.float32, shape=(84,84,4)) # TODO: pretty sure that shape isn't right
-        self.input = tf.placeholder(tf.float32, shape=(1, 84,84,4)) # TODO: pretty sure that shape isn't right
+        #self.input = tf.placeholder(tf.float32, shape=(1, 84,84,4)) # TODO: pretty sure that shape isn't right
+        self.input = tf.placeholder(tf.float32, shape=(1,84,84,1)) # TODO: pretty sure that shape isn't right
 
         # convolutional layers
 
-        # TODO: need weights and biases between each layer right?? Or is this automatically handled?
-        # NOTE: I assume yes, tf docs mentione "trainable" variable which adds variables to graph?
-        
         # 32 filters, kernel size of 8, stride of 4
         self.conv1 = tf.layers.conv2d(self.input, 32, 8, 4, activation=tf.nn.relu, name='conv1')
         
@@ -59,9 +65,6 @@ class Agent:
         
         # 64 filters, kernel size of 3, stride of 1
         self.conv3 = tf.layers.conv2d(self.conv2, 64, 3, 1, activation=tf.nn.relu, name='conv3')
-
-        # NOTE: think I might need to flatten output of conv3?
-
         self.conv3_out = tf.reshape(self.conv3, [-1, 3136], name='conv3_flatten')
 
 
@@ -73,32 +76,61 @@ class Agent:
 
             self.fc_out = tf.nn.relu_layer(self.conv3_out, self.fc_w, self.fc_b, name='fc_out')
 
-        # output layer 
+        # output layer (for space invaders, 6 possible outputs)
         with tf.name_scope('output'):
             self.out_w = tf.Variable(tf.random_normal([512, 6]), name='out_weights')
             self.out_b = tf.Variable(tf.random_normal([6]), name='out_biases')
 
             self.output = tf.matmul(self.fc_out, self.out_w) + self.out_b
-        
-        
-        # NOTE: 6 possible outputs for space invaders
-        # final 
 
+        #self.summary_op = tf.summary.tensor_summary("output", self.output)
+
+        #for i in range(self.output.get_shape()[0]):
+        #    for j in range(self.output.get_shape()[1]):
+        #        tf.summary.scalar("output_" + str(i) + "_" + str(j), self.output[i, j])
 
         self.merged_summaries = tf.summary.merge_all()
         self.sess.run(tf.global_variables_initializer())
         self.train_writer = tf.summary.FileWriter('../tensorboard_data/cnn_atari2' , self.sess.graph)
-        #self.test_writer = tf.summary.FileWriter(self.logdir + self.name)
         self.train_writer.add_graph(self.sess.graph)
-        #self.test_writer.add_graph(self.session.graph) 
-        
+        #self.train_writer.add_summary(self.merged_summaries)
+        #self.train_writer.add_summary(self.sess.run(summary_op), 1))
+
+    # if none is returned, take random action
+    def act(self, frameInput):
+        exploreOrNo = random.uniform(0,1)
+        if exploreOrNo < self.epsilon: return None
+        else: return np.argmax(self.sess.run([self.output], feed_dict={self.input: frameInput}))
+            
+        #result = self.sess.run([self.output], feed_dict={self.input: frameInput})
+        #return result
 
 a = Agent()
 a.buildGraph()
 print("done")
 
 e = Environment()
-frame0 = e.env.reset()
-imsave('frame0.png', frame0)
-frame1 = e.preprocessFrame(frame0)
-imsave('frame1.png', frame1)
+state = e.getInitialState()
+
+#a.sess.run([a.output, a.summary_op], feed_dict={a.input: state})
+#result, summary = a.sess.run([a.output, a.summary_op], feed_dict={a.input: state})
+#result = a.sess.run([a.output], feed_dict={a.input: state})
+result = a.act(state)
+action = np.argmax(result)
+print(result)
+print(action)
+
+done = False
+while not done:
+    newstate, reward, done = e.act(action) 
+    #action = np.argmax(a.act(newstate))
+    e.env.render()
+
+#a.train_writer.add_summary(summaries)
+
+
+#frame0 = e.env.reset()
+#imsave('frame0.png', frame0)
+#frame1 = e.preprocessFrame(frame0)
+#imsave('frame1.png', frame1)
+
